dc32fe609edb3a15a740c786683fc87eab429b4b
Willie Lin <willielin@williede-macbook-pro.local>
2018-06-27 01:18:10 +0800

    Initial Commit

---
+++ bangboom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bangboom
+//
+//  Created by Willie Lin on 2018/6/27.
+//  Copyright © 2018年 Willie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
